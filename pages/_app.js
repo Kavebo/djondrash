2988ae4d78f5510bdd7099ff20dccccca8de1692
Jozef Kavecansky <jozef.kavecansky@gmail.com>
2020-11-20 10:51:00 +0100

    feat(Loading): add Nprogress to side

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,22 @@
 import '../styles/globals.css';
 
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
 import { LanguageProvider } from '../languages';
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: 'ease',
+  speed: 800,
+  showSpinner: true,
+});
+
+Router.events.on('routeChangeStart', () => {
+  NProgress.start();
+});
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
