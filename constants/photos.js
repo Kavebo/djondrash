362254131ca55edcfe97570def18d664c41691a5
djondrash <86789617+djondrash@users.noreply.github.com>
2021-08-12 22:29:01 +0200

    Update photos.js

--- constants/photos.js
+++ constants/photos.js
@@ -1,5 +1,16 @@
 import img1 from '../assets/images/gallery/img_1.png';
 import img2 from '../assets/images/gallery/img_2.png';
+import img3 from '../assets/images/gallery/img_3.png';
+import img4 from '../assets/images/gallery/img_4.png';
+import img5 from '../assets/images/gallery/img_5.png';
+import img6 from '../assets/images/gallery/img_6.png';
+import img7 from '../assets/images/gallery/img_7.png';
+import img8 from '../assets/images/gallery/img_8.png';
+import img9 from '../assets/images/gallery/img_9.png';
+import img10 from '../assets/images/gallery/img_10.png';
+import img11 from '../assets/images/gallery/img_11.png';
+import img12 from '../assets/images/gallery/img_12.png';
+import img13 from '../assets/images/gallery/img_13.png';
 
 export const photos = [
   {
@@ -12,10 +23,10 @@ export const photos = [
     width: 3,
     height: 2,
   },
-  {
-    src: 'https://source.unsplash.com/epcsn8Ed8kY/600x799',
-    width: 3,
-    height: 4,
+   {
+    src: img3,
+    width: 4,
+    height: 3,
   },
   {
     src: 'https://source.unsplash.com/NQSWvyVRIJk/800x599',
